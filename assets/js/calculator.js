9976f4e1e0b3ed6e5ed40507e6fd6a5ad8214754
John <john@linkodyssey.com>
2020-10-28 01:48:21 -0500

    Add reversePolish function

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,8 +1,42 @@
 jQuery(document).ready(function($) {
+	function reversePolish(newExpr) {
+		let expr = newExpr.split(" ");
+		let stack =[];
+
+		if (expr === '') {
+			return 0;
+		}
+
+		for (let i = 0; i < expr.length; i++) {
+			if ( !isNaN(expr[i]) && isFinite(expr[i]) ) {
+				stack.push(expr[i]);
+			} else {
+				let a = stack.pop();
+				let b = stack.pop();
+
+				if (expr[i] === "+") {
+					stack.push(parseInt(a) + parseInt(b));
+				} else if (expr[i] === "-") {
+					stack.push(parseInt(b) - parseInt(a));
+				} else if (expr[i] === "*") {
+					stack.push(parseInt(a) * parseInt(b));
+				} else if (expr[i] === "/") {
+					stack.push(parseInt(b) / parseInt(a));
+				}
+			}
+		}
+
+		if (stack.length > 1) {
+			return "ERROR";
+		} else {
+			return stack[0];
+		}
+	}
+
 	$('.form-signin').submit(function(e) {
 		e.preventDefault();
 		
-		let input = $('#inputText').val();
+		let input = reversePolish( $('#inputText').val() );
 
 		$('.response').text(input);
 	});
