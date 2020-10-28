97603587902657c2dd9b39fdcc191a3eeb73121e
John <john@linkodyssey.com>
2020-10-28 00:40:26 -0500

    Retrieve form input and prevent default form submit

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function($) {
-	$()
+	$('.form-signin').submit(function(e) {
+		e.preventDefault();
+		
+		let input = $('#inputText').val();
+
+		$('.response').text(input);
+	});
 });
